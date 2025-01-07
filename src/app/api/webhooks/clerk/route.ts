cb5461cad65d6925baf5f64cc8ae85c54004a0e3
Growddy <eedwardjiang@gmail.com>
2025-01-07 18:59:24 -0500

    Deleted unnecessary import

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,7 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
-import type { ClerkClient } from "@clerk/backend";
 import { createUser } from '../../../../../actions/user.action'
 import { NextResponse } from 'next/server'
 
