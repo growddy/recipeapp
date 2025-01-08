2cc504fae1bfd33a525bc7b297343f23297a5748
Growddy <eedwardjiang@gmail.com>
2025-01-07 23:43:46 -0500

    Fix for redirect to sign in, protect the my profile tab

--- src/app/(topbar)/myprofile/page.tsx
+++ src/app/(topbar)/myprofile/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { useUser } from '@clerk/nextjs'
+import { RedirectToSignIn, useUser } from '@clerk/nextjs'
 import React from "react";
 
 const ClientPage = () => {
   const { isLoaded, isSignedIn, user } = useUser();
 
   if (!isLoaded || !isSignedIn) {
-    return null;
+    return <RedirectToSignIn />;
   }
   return (
     <div className="h-full flex flex-col items-center justify-center text-2xl">
